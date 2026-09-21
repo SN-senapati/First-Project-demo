280af98b10f8f8ce6c5e5d371e49c07582de9b36
Sudipta <senapatisudipti@gmail.com>
2026-09-21 22:26:25 +0530

    First priority added

--- hello.js
+++ hello.js
@@ -1,2 +1,14 @@
 let x = 10
 console.log(x)
+
+
+// First priority
+function chaita(){
+    let x = 20
+    function chaita2(){
+        let y = 30
+        console.log(x)
+    }
+    chaita2()
+}
+chaita()
