70caee8615ae9d9bde55191dc42e81abb073ce31
Sudipta Narayan Senapati <135970249+SN-senapati@users.noreply.github.com>
2026-09-21 23:04:24 +0530

    Update research.js

--- research.js
+++ research.js
@@ -1,15 +1,1 @@
-<<<<<<< Updated upstream
 
-function outer(){
-  let x = 20;
-  function inner(){
-   console.log(x)
-}
-inner()
-}
-outer()
-=======
-let x = 10
-let y = 20
-let sum = 
->>>>>>> Stashed changes
