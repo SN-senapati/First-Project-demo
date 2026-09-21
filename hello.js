2b2d111cffe9d8a43889e78d54c1ceec63116e25
Sudipta <senapatisudipti@gmail.com>
2026-09-21 22:35:33 +0530

    delted

--- hello.js
+++ hello.js
@@ -1,22 +1,5 @@
 let x = 10
 console.log(x)
 
-<<<<<<< Updated upstream
 
-// First priority
-function chaita(){
-    let x = 20
-    function chaita2(){
-        let y = 30
-        console.log(x)
-    }
-    chaita2()
-}
-chaita()
-=======
-// not first priority
-function chunia(){
-    let value = 10;
 
-}
->>>>>>> Stashed changes
