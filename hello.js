c414cd7535b6f5d4300803d7c3fc7defc86a62dc
Sudipta <senapatisudipti@gmail.com>
2026-09-21 22:43:43 +0530

    first priority completed

--- hello.js
+++ hello.js
@@ -3,3 +3,14 @@ console.log(x)
 
 
 
+// First priority
+function outer(){
+    var z = 10
+    function inner(){
+        console.log(z)
+        var y = 20
+        console.log(y)
+    }
+    inner()
+}
+outer()
