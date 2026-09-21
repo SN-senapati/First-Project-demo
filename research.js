3dc181fecb8ac81da3474c6eeb950d0f47c84c57
Sudipta <senapatisudipti@gmail.com>
2026-09-21 22:59:30 +0530

    added easily

--- research.js
+++ research.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 function outer(){
   let x = 20;
@@ -7,3 +8,8 @@ function outer(){
 inner()
 }
 outer()
+=======
+let x = 10
+let y = 20
+let sum = 
+>>>>>>> Stashed changes
